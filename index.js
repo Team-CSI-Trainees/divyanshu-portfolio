326bf4dc943127625c8d67088ae8999ff4d21dad
divyansh453 <104566386+divyansh453@users.noreply.github.com>
2022-10-01 12:35:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+let para=document.getElementById("hidemenu");
+para.style.display="none";
+function hide() {
+    if(para.style.display=="none")
+    para.style.display="flex";
+    else
+    para.style.display="none";
+}
