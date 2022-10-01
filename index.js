f4ad633f87ac00b90e921f425c00cf9e63951b82
divyansh453 <104566386+divyansh453@users.noreply.github.com>
2022-10-01 12:48:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 let para=document.getElementById("hidemenu");
-para.style.display="none";
+para.style.display="flex";
 function hide() {
     if(para.style.display=="none")
     para.style.display="flex";
     else
     para.style.display="none";
 }
+para.style.display="flex";
